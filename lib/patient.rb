b4fdac03b27fb1b7197e1cd74a50b1dfb9eb9f88
Andrew Bay <daddnbay@yahoo.com>
2019-07-26 15:59:24 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,6 +12,9 @@ class Patient
     @@all
   end
   
+  def new_appointment(doctor, date)
+    
+  
   def appointments
     Appointment.all.select do |appointment|
      appointment.patient == self
